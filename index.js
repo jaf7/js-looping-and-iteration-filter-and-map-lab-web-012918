03736c5d45e7d72e321a87d7edcd00e5d62ddf9b
jaf7 <janthonyfields@gmail.com>
2018-03-11 18:34:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, num) {
+  return drivers.filter(function(driver) {
+    return driver['revenue'] > num;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, num) {
+  return driversWithRevenueOver(drivers, num).map(function(driver) {
+    return driver['name'];
+  });
+}
+
+function exactMatch(drivers, obj) {
+  return drivers.filter(function(driver) {
+    for (const attribute in driver) {
+      if (driver[attribute] === obj[attribute]) {
+        return true;
+      }
+    }
+  });
+}
+
+function exactMatchToList(drivers, obj) {
+  return exactMatch(drivers, obj).map(function(driver) {
+    return driver['name'];
+  });
+}
